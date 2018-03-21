5eee865a1b0222c8e73d20d0dbe99bd9a6699b76
kristo.vanderflit <kristo.vanderflit@khk.ee>
2018-03-21 13:21:06 +0200

    linkimine tootab

--- controllers/posts.php
+++ controllers/posts.php
@@ -15,10 +15,10 @@ class posts extends Controller
         $this->users = get_all("SELECT * FROM users");
     }
 
-    function view()
+    function  view()
     {
         $post_id = $this->params[0];
-        $this->post = get_one("SELECT * FROM post NATURAL JOIN user WHERE post_id='$post_id'");
+        $this->post = get_first("SELECT * FROM post NATURAL JOIN users WHERE post_id='$post_id'");
     }
 
     /**
--- views/posts/posts_index.php
+++ views/posts/posts_index.php
@@ -1,12 +1,7 @@
-<div class="span8">
+<div class="container">
     <?foreach($posts as $post):?>
-
         <a href="<?=BASE_URL?>posts/view/<?=$post['post_id']?>"><h1><?=$post['post_subject']?></h1></a>
-
         <p><?=$post['post_text']?></p>
-    <div>
-        <span class="badge badge-success"><?=$post['post_created']?></span><div class="pull-right"><span class="label">alice</span> <span class="label">story</span> <span class="label">blog</span> <span class="label">personal</span></div
-    </div>
+        <span class="badge badge-success">Posted <?=$post['post_created']?></span><div class="pull-right"><span class="label">alice</span> <span class="label">story</span> <span class="label">blog</span> <span class="label">personal</span></div>
     <?endforeach?>
 </div>
-
