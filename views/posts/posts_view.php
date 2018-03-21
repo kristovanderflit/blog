46292639f40db672b684a810cae801043e890699
kristo.vanderflit <kristo.vanderflit@khk.ee>
2018-03-21 13:22:28 +0200

    linkimine tootab nüüd korralikult. avaneb see post mis vaja.

--- views/posts/posts_view.php
+++ views/posts/posts_view.php
@@ -1,7 +1,5 @@
 <div class="container">
-    <?foreach($posts as $post):?>
         <h1><?=$post['post_subject']?></h1></a>
         <p><?=$post['post_text']?></p>
         <span class="badge badge-success">Posted <?=$post['post_created']?></span><div class="pull-right"><span class="label">alice</span> <span class="label">story</span> <span class="label">blog</span> <span class="label">personal</span></div>
-    <?endforeach?>
 </div>
